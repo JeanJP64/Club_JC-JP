8362724a74fe779a9a2eaa583a58e23a0a1a2f1e
JamesDCG <jamesdcg9@hotmail.com>
2025-06-26 16:30:58 -0500

    Main

--- src/club/Main.java
+++ src/club/Main.java
@@ -1,7 +1,6 @@
 package club;
 
 import java.util.Scanner;
-import club.Socio.Tipo;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
